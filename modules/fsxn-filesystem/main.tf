8e4809f4c5051f8093fcf4edfe6d5b2fa12093de
yshimizu37 <yshimizu@netapp.com>
2022-10-24 01:19:37 +0000

    fix fsxn modules

--- modules/fsxn-filesystem/main.tf
+++ modules/fsxn-filesystem/main.tf
@@ -1,6 +1,9 @@
 variable "subnet_ids" {}
 variable "preferred_subnet_id" {}
 variable "fsx_admin_password" {}
+variable "throughput_capacity" {}
+variable "deployment_type" {}
+variable "storage_capacity" {}
 
 resource "aws_fsx_ontap_file_system" "aws-fsx01" {
   storage_capacity    = var.storage_capacity
